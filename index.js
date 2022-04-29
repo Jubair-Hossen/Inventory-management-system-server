3b22246f9ccbd148899a6f942efb1b551b752f16
Jubair <jubairhossen55@gmail.com>
2022-04-29 21:11:24 +0600

    add product to inventory api created

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//use midlewares
+app.use(cors());
+app.use(express.json());
 
 const uri = "mongodb+srv://jubair1:kvTt4bMebEaqbxRY@cluster0.phm7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -12,19 +16,12 @@ async function run() {
         await client.connect();
         const productCollection = client.db("warehouse").collection("products");
 
-        app.get('/users', async (req, res) => {
-            const query = {};
-            const cursor = productCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users)
+        // add to inventory api
+        app.post('/add-products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send({ result })
         })
-        // const pd = {
-        //     name: 'laptop',
-        //     price: 200
-        // }
-
-        // const result = await productCollection.insertOne(pd);
-        // console.log("pd added");
 
     }
     finally {
