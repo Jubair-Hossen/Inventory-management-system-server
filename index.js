442691f28df24664dca4f39555e992ec0e84d1da
Jubair <jubairhossen55@gmail.com>
2022-04-29 12:43:05 +0600

    mongo db connected with server side

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+const uri = "mongodb+srv://jubair1:kvTt4bMebEaqbxRY@cluster0.phm7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("warehouse").collection("products");
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
+        })
+        // const pd = {
+        //     name: 'laptop',
+        //     price: 200
+        // }
+
+        // const result = await productCollection.insertOne(pd);
+        // console.log("pd added");
+
+    }
+    finally {
+        // do some thing
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("welcome to my warehous-management-systems")
