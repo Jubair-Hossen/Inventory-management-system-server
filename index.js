b22bd490803c23a1407fa47f859586c08d2839b9
Jubair <jubairhossen55@gmail.com>
2022-05-06 16:40:37 +0600

    delete api created

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ async function run() {
             res.send(result);
         })
 
+        // delete api
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
     }
     finally {
         // do some thing
